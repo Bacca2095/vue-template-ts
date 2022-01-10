4d89f79fb702f1eddb889bdbf23640904fed1ce0
Cesar Bacca <bacca2095@gmail.com>
2022-01-10 02:50:40 -0500

    feat: agregado componente de boton

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button v-bind="$attrs" :class="`btn ${currentVariant} ${currentSize}`" ><slot/></button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'b-button',
+  props: {
+    text: String,
+    variant: String,
+    size: String,
+  },
+  setup(props) {
+    const currentVariant = ref<string>(`btn-${props.variant}`);
+    const currentSize = ref<string>(`btn-${props.size}`);
+    return { currentVariant, currentSize };
+  },
+});
+</script>
