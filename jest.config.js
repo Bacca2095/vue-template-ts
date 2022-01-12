f39fcabb415d41c1f616b611b873b92808a822a8
Cesar Bacca <bacca2095@gmail.com>
2022-01-12 14:46:05 -0500

    fix: correccion de test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**/**/*.vue', './src/**/**/*.ts'],
+  coverageReporters: ['lcov'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
   },
--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -1,15 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router';
 import { shallowMount } from '@vue/test-utils';
 import App from '@/App.vue';
 import store from '@/store/index';
-import router from '@/router/index';
+import Home from '@/views/Home.vue';
 
 describe('App.vue', () => {
-  it('Render initial app', () => {
+  it('Render initial app', async () => {
+    const router = createRouter({
+      history: createWebHistory(),
+      routes: [{ path: '/', component: Home }],
+    });
+
     const wrapper = shallowMount(App, {
       global: {
         plugins: [store, router],
       },
     });
+    await router.isReady();
     expect(wrapper).toBeTruthy();
   });
 });
