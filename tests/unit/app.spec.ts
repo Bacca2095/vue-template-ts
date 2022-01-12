d4067f0ea600886588f521d3f48cc6af1c940558
Cesar Bacca <bacca2095@gmail.com>
2022-01-12 14:53:58 -0500

    test: agregado router a app

--- tests/unit/app.spec.ts
+++ tests/unit/app.spec.ts
@@ -1,16 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
 import { shallowMount } from '@vue/test-utils';
 import App from '@/App.vue';
 import store from '@/store/index';
-import Home from '@/views/Home.vue';
+import router from '@/router/index';
 
 describe('App.vue', () => {
   it('Render initial app', async () => {
-    const router = createRouter({
-      history: createWebHistory(),
-      routes: [{ path: '/', component: Home }],
-    });
-
     const wrapper = shallowMount(App, {
       global: {
         plugins: [store, router],
