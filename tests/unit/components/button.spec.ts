b0fcaa2b12fa2bc5dbadd3a8091b406e70c656d4
Cesar Bacca <bacca2095@gmail.com>
2022-01-16 12:32:57 -0500

    fix: correccion de test Button

--- tests/unit/components/button.spec.ts
+++ tests/unit/components/button.spec.ts
@@ -3,12 +3,11 @@ import Button from '@/components/Button.vue';
 
 describe('Button.vue', () => {
   it('Render button', () => {
-    const text = 'Text';
     const variant = 'primary';
     const size = 'sm';
     const wrapper = shallowMount(Button, {
-      props: { text, variant, size },
+      props: { variant, size },
     });
-    expect(wrapper.classes()).toEqual(['btn', 'btn-primary', 'btn-sm']);
+    expect(wrapper.classes()).toContain('text-sm');
   });
 });
